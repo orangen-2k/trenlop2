005367ee42eff4e183b67080914da1ed3bd9dca9
orangen-2k <orangen2000@gmail.com>
2020-03-24 15:53:11 +0700

    3 files modified, 7 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { HotelsComponent } from './hotels/hotels.component';
+import { HotelService } from './hotel.service';
+import { HotelDetailComponent } from './hotel-detail/hotel-detail.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule, FormsModule, HttpClientModule,
+    RouterModule.forRoot([
+      {path: '', component: HotelsComponent},
+      {path: 'detail/:hotelId', component: HotelDetailComponent}
+    ])
+  ],
+  declarations: [ AppComponent, HotelsComponent, HotelDetailComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [HotelService]
 })
 export class AppModule { }
