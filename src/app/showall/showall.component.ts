c8af1fee85cb203ab9e159d0895a1a5f4fa904dc
orangen-2k <orangen2000@gmail.com>
2020-04-22 19:53:42 +0700

    3 files modified

--- src/app/showall/showall.component.ts
+++ src/app/showall/showall.component.ts
@@ -7,27 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowallComponent implements OnInit {
 
-  itemsT = [];
-
-  pageOfItems: Array<any>;
-
-  constructor() {}
-
+   title = 'custom-pagination-angular';
+  data = [];
+  pagedItems: Array<any>;
+  constructor() { }
   ngOnInit() {
-    var arr = Array(40);
-    this.itemsT = arr
-      .fill({
-        id: 0,
-        name: 0
-      })
-      .map((x, i) => ({
-        id: i + 1,
-        name: `Iteam ${i + 1}`
-      }));
+    this.data = Array(1000).fill(0).map((x, i) => ({ id: (i + 1), name: `Item Paged ${i + 1}`, product: `Product ${i + 1}` }));
   }
-
-  onChangePage(pageOfItems: Array<any>) {
-    console.log(pageOfItems);
-    this.pageOfItems = pageOfItems;
+  beginPagination(pagedItems: Array<any>) {
+    this.pagedItems = pagedItems;
   }
 }
