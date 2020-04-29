f61df9833812e8fd6c5e7046f2ee493ad96e0b94
orangen-2k <orangen2000@gmail.com>
2020-04-29 23:00:08 +0700

    2 files modified, 2 added

--- src/app/showall/showall.component.ts
+++ src/app/showall/showall.component.ts
@@ -1,6 +1,4 @@
-import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import {SchoolsService} from '../schools.service';
 
 @Component({
   selector: 'app-showall',
@@ -10,21 +8,9 @@ import {SchoolsService} from '../schools.service';
 export class ShowallComponent implements OnInit {
 
   constructor(
-    private activeRoute: ActivatedRoute,
-    private route: Router,
-    private schoolsService: SchoolsService
     ) { }
 
   ngOnInit() {
-    this.schoolsService.getSchools().subscribe(data => {
-      console.log(data);
-      this.schools = data;
-    });
-  }
-
-  schools = [];
-  array: Array<any>;
-  beginPagination(array: Array<any>) {
-    this.array = array;
+    
   }
 }
